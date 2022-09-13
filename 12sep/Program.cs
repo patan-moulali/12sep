cf43c7930eeceb5100ce3021e1edfe7d18a658b4
Patan Moulali <patan.moulali@mphasis.com>
2022-09-13 12:58:54 +0530

    Add project files.

---
+++ 12sep/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace _12sep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @" C:\Users\LENOVO\source\repos\12sep\ClassLibrary1\bin\Debug\ClassLibrary1.dll";
+            Assembly asm = Assembly.LoadFrom(path);
+
+            Type[] types = asm.GetTypes();
+            Type t = null;
+
+            foreach (var item in types)
+            {
+                //Console.WriteLine("name= " + item.Name);//classname
+                //Console.WriteLine("Full Name= " + item.FullName);//assemblyname/namespacename and class
+
+                //Console.WriteLine("IS Class=" + item.IsClass);
+                //Console.WriteLine("IS Absatract=" + item.IsAbstract);
+                //Console.WriteLine("Whether public " + item.IsPublic);
+
+
+                //Console.WriteLine("-----------------");
+                //MemberInfo[] minfo = item.GetMembers();
+
+                //foreach (var minfoitem in minfo)
+                //{
+
+                //    Console.WriteLine("Member Info Name= " + minfoitem.Name);
+
+                //    Console.WriteLine("Declaring type=" + minfoitem.DeclaringType);
+                //}
+                //Console.WriteLine("---------------------");
+                //MethodInfo[] methodInfos = item.GetMethods();
+                //foreach (var methodInfosItem in methodInfos)
+                //{
+
+                //    Console.WriteLine("name=" + methodInfosItem.Name);
+                //    Console.WriteLine("IS PUBLIC=" + methodInfosItem.IsPublic);
+                //    Console.WriteLine("IS STATIC =" + methodInfosItem.IsStatic);
+
+                //    ParameterInfo[] pinfo = methodInfosItem.GetParameters();
+                //    Console.WriteLine("---Add method Parameters----");
+
+
+                //    foreach (var par in pinfo)
+                //    {
+                //        Console.WriteLine(par.Name);
+                //        Console.WriteLine(par.Position);
+                //        Console.WriteLine(par.ParameterType);
+
+
+                //    }
+
+                //}
+
+                t = asm.GetType(item.FullName);
+
+                object obj = Activator.CreateInstance(t);
+
+
+                item.InvokeMember("SqrRt", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[] { 16 });
+
+                item.InvokeMember("Cube", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[] { 3 });
+
+                break;
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        public double Number { get; set; }
+
+        public void SqrRt(double n)
+        {
+            Number = n;
+            double ans = Math.Sqrt(n);
+            Console.WriteLine("The square root of number is " + ans);
+
+
+        }
+
+        public void Cube(double no)
+        {
+            Number = no;
+            double answer = no * no * no;
+            Console.WriteLine("The cube of number is " + answer);
+        }
+    }
+}
